49f0f6a63d9ff58154c0cc9fbfc68a54cc6f75ed
Peter Wonham <peter_wonham@whirlpool.com>
2020-06-02 16:40:07 +0100

    Second Commit

--- ToastLibrary/src/main/java/com/whirpool/toastlibrary/ToasterMessage.kt
+++ ToastLibrary/src/main/java/com/whirpool/toastlibrary/ToasterMessage.kt
@@ -1,9 +1,18 @@
 package com.whirpool.toastlibrary
 
 import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.toasttest.*
 
-class ToasterMessage {
+class ToasterMessage :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.toasttest)
+        button.setOnClickListener { s(this,toast.toString()) }
+    }
     fun s(c:Context,message:String){
         Toast.makeText(c,message,Toast.LENGTH_LONG).show()
     }
--- app/src/main/java/com/whirpool/toastexample/MainActivity.kt
+++ app/src/main/java/com/whirpool/toastexample/MainActivity.kt
@@ -2,10 +2,12 @@ package com.whirpool.toastexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.whirpool.toastlibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToasterMessage().s(this,"Hello")
     }
 }
